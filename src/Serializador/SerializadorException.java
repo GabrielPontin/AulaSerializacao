083c71accbef2c4dc243c5226e0a0f50d27f8c83
CLIENTE <CLIENTE@Pontin>
2016-05-11 21:13:50 -0300

    Metodo para geral um Exception e dizer

--- src/Serializador/SerializadorException.java
+++ src/Serializador/SerializadorException.java
@@ -2,11 +2,15 @@ package Serializador;
 
 public class SerializadorException extends Exception {
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 5950646091103095787L;
 
+	public SerializadorException(Exception e){
+		super(e);
+	} 
+	
+	public SerializadorException(String string){
+		super(string);
+	}
 	
 	
 }
